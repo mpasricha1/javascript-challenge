cfb0791c441c182d8cd5aee1e459ede21b696143
Mark Pasricha <markp862@gmail.com>
2020-08-05 21:08:59 -0400

    done for the day

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,29 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+function generateTableHead(table,data1) { 
+	var thead = table.createTHead(); 
+	var row = thead.insertRow()
+	for (let key of data1){ 
+		let th=document.createElement("th"); 
+		let text = document.createTextNode(key); 
+		th.appendChild(text); 
+		row.appendChild(th);
+	}
+}	
+
+function generateTable(table, data){ 
+	for(let element of data){ 
+		let row = table.insertRow();
+		for (key in element){ 
+			let cell = row.insertCell();
+			let text = document.createTextNode(element[key]);
+			cell.appendChild(text)
+		}
+	}
+}
+
+let table = document.querySelector("table"); 
+let data1 = Object.keys(tableData[0]);
+generateTableHead(table,data1)
+generateTable(table,tableData)
