f6a677b4f339cf326639e2c4d8899ec2edbd07b8
Mark Pasricha <markp862@gmail.com>
2020-08-12 21:14:20 -0400

    finished functionality

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,11 @@
 // from data.js
 var tableData = data;
 var date = d3.select("#datesearch")
-var filterButton = d3.select("#filterscreen")
+var city = d3.select("#citysearch")
+var state = d3.select("#statesearch")
+var country = d3.select("#countrysearch")
+var shape = d3.select("#shapesearch")
+var clear = d3.select("#clearfilters")
 
 // YOUR CODE HERE!
 //Function to generate the rows of the table
@@ -9,7 +13,6 @@ function generateTable(data){
 	var tbody = d3.select("tbody");
 	$("#tablebody tr").remove();
 	data.forEach(function(ufoData){
-
 		var row = tbody.append("tr"); 
 		Object.entries(ufoData).forEach(function([key,value]){
 			var cell = row.append("td"); 
@@ -18,16 +21,38 @@ function generateTable(data){
 	});
 };
 	
-
 generateTable(tableData);
+
 date.on("change", function(){
 	var filterDate = d3.event.target.value;
-	var filterResults = data.filter(date => date.datetime === filterDate);
-
-	generateTable(filterResults);
+	generateTable(data.filter(row => row.datetime === filterDate.trim()));
 });
 
-filterButton.on("click", function(){
-	console.log("Button Clicked")
-	document.getElementById("overlay").style.display = "block"
-} )
+city.on("change", function(){
+	var filterDate = d3.event.target.value;
+	generateTable(data.filter(row => row.city === filterDate.toLowerCase().trim()));
+});
+
+state.on("change", function(){
+	var filterDate = d3.event.target.value;
+	generateTable(data.filter(row => row.state === filterDate.toLowerCase().trim()));
+});
+
+country.on("change", function(){
+	var filterDate = d3.event.target.value;
+	generateTable(data.filter(row => row.country === filterDate.toLowerCase().trim()));
+});
+
+shape.on("change", function(){
+	var filterDate = d3.event.target.value;
+	generateTable(data.filter(row => row.shape === filterDate.toLowerCase().trim()));
+});
+
+clear.on("click", function(){
+	document.getElementById("datesearch").value=""
+	document.getElementById("citysearch").value=""
+	document.getElementById("statesearch").value=""
+	document.getElementById("countrysearch").value=""
+	document.getElementById("shapesearch").value=""
+	generateTable(data)
+});
